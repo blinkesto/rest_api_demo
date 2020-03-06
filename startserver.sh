a0c8039464207d1415419988000e430b33879e58
koydo <koydooley@gmail.com>
2020-03-06 10:29:21 -0600

    Update startserver for aws

--- startserver.sh
+++ startserver.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # rm dist/rest_api_demo-1.0.0* && python setup.py sdist bdist_wheel && pip uninstall -y rest-api-demo && pip install dist/rest_api_demo-1.0.0.tar.gz && python reset_db.py
-if argv[1] == 'aws'; then
+if [ $1 == 'aws' ]; then
   export FLASK_SERVER_NAME='ec2-3-18-92-220.us-east-2.compute.amazonaws.com:8888'
 else
   export FLASK_SERVER_NAME='10.58.143.137:8888'
