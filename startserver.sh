d2c372febe401932e033c86a68cbad83baecf9a2
koydo <koydooley@gmail.com>
2020-03-06 10:24:55 -0600

    Update startserver for aws

--- startserver.sh
+++ startserver.sh
@@ -5,6 +5,7 @@ if argv[1] == 'aws'; then
   export FLASK_SERVER_NAME='ec2-3-18-92-220.us-east-2.compute.amazonaws.com:8888'
 else
   export FLASK_SERVER_NAME='10.58.143.137:8888'
+fi
 
 export PYTHONPATH=`pwd`
 python rest_api_demo/flaskr.py 2>&1 |tee -a debug.out
