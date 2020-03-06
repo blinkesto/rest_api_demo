bf84cabf8b4b7023efc9bb19b797ced01122d267
koydo <koydooley@gmail.com>
2020-03-05 20:00:15 -0600

    Update python path

--- startserver.sh
+++ startserver.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+# rm dist/rest_api_demo-1.0.0* && python setup.py sdist bdist_wheel && pip uninstall -y rest-api-demo && pip install dist/rest_api_demo-1.0.0.tar.gz && python reset_db.py
+export PYTHONPATH=`pwd`
 python rest_api_demo/flaskr.py 2>&1 |tee -a debug.out
